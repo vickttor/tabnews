22eb9a51a46e6f6b186520b112425467484c6c3b
Victor H. Silva <victor.silvaPC@hotmail.com>
2024-06-27 19:56:12 -0300

    feat: install jest and configure test scripts

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,26 @@
 export default function Home() {
   return (
-    <main style={{
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      width: "100dvw",
-      height: "100dvh",
-      overflowX: "hidden",
-    }}>
-      <h1 style={{ textAlign: "center" }}>🌐 Tabnews Clone - Under Development 🔩 🚧</h1>
-      <span stye={{ color: "#970DC9" }}>Learning and coding to get a better life.</span>
-      <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
+    <main
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100dvw",
+        height: "100dvh",
+        overflowX: "hidden",
+      }}
+    >
+      <h1 style={{ textAlign: "center" }}>
+        🌐 Tabnews Clone - Under Development 🔩 🚧
+      </h1>
+      <span stye={{ color: "#970DC9" }}>
+        Learning and coding to get a better life.
+      </span>
+      <script
+        src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs"
+        type="module"
+      ></script>
 
       <dotlottie-player
         src="https://lottie.host/085ca238-4e03-4ed6-8093-09176802abc8/9qf86bKypT.json"
@@ -27,8 +36,10 @@ export default function Home() {
 
       <footer>
         <span>&copy; Made by </span>
-        <a target="_blank" href="https://github.com/vickttor">vickttor</a>
+        <a target="_blank" href="https://github.com/vickttor">
+          vickttor
+        </a>
       </footer>
     </main>
-  )
+  );
 }
