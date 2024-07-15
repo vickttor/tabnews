f1277f84aac8b3d0340abe1cce3b04d5d8f39d67
Victor H. Silva <victor.silvaPC@hotmail.com>
2024-07-15 18:03:02 -0300

    add database.js file and create environment variables

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-export default function onRequest(request, response) {
+import database from "../../../../infra/database.js"
+
+export default async function onRequest(request, response) {
+  const result = await database.query('SELECT 1 + 1 as sum;')
+  console.log(result.rows);
   response.status(200).json({
     message: "Endpoint de Status"
   })
