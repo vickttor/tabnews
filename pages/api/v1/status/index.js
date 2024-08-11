06f8e92e39e0240aa895bf6845b062a5ed88dacb
Victor H. Silva <victor.silvaPC@hotmail.com>
2024-08-11 14:53:57 -0300

    feat: finish the /api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,30 @@
 import database from "infra/database.js";
 
-export default async function onRequest(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+export default async function status(request, response) {
+
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const databaseMaxConnectionsValue = parseInt(databaseMaxConnectionsResult.rows[0].max_connections);
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  });
+  const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
+
   response.status(200).json({
-    message: "Endpoint de Status",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: databaseMaxConnectionsValue,
+        opened_connections: databaseOpenedConnectionsValue
+      }
+    }
   });
 }
