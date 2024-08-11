7e59ee942b2d65ba532e1598a4dcb33402149f17
Victor H. Silva <victor.silvaPC@hotmail.com>
2024-08-11 11:56:03 -0300

    chore: run lint:fix

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,3 @@
-
 import { Client } from "pg";
 
 async function query(queryObject) {
@@ -7,7 +6,7 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD
+    password: process.env.POSTGRES_PASSWORD,
   });
 
   await client.connect();
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,9 @@
-import database from "infra/database.js"
+import database from "infra/database.js";
 
 export default async function onRequest(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;')
+  const result = await database.query("SELECT 1 + 1 as sum;");
   console.log(result.rows);
   response.status(200).json({
-    message: "Endpoint de Status"
-  })
+    message: "Endpoint de Status",
+  });
 }
