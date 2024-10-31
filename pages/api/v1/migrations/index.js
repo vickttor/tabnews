2187234f52d039220a1b153a114ceecb3980ece6
Victor H. Silva <victor.silvaPC@hotmail.com>
2024-10-31 15:55:50 -0300

    feat: :tada adds  endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,31 +1,37 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
-
+import database from "infra/database";
 export default async function migrations(request, response) {
-  if (request.method === 'GET') {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations"
-    })
+  const dbClient = await database.getNewClient();
 
-    return response.status(200).json(migrations);
+  const defaultMigrationsOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations"
+  };
+
+  if (request.method === 'GET') {
+    const pendingMigrations = await migrationRunner(defaultMigrationsOptions)
+    dbClient.end();
+    return response.status(200).json(pendingMigrations);
   }
 
   if (request.method === 'POST') {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: false,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations"
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationsOptions,
+      dryRun: false
     })
 
-    return response.status(200).json(migrations);
+    dbClient.end();
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+
+    return response.status(200).json(migratedMigrations);
   }
 
   return response.status(405).json({ message: 'Method not allowed' });
